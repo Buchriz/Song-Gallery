2bf477fde58a1d48c99f0d59c351b0419a903784
Buchriz <s88100@nhs.co.il>
2024-06-11 18:17:46 +0300

    almost finished design

---
+++ app/src/main/java/com/example/songgallery/Album_List.java
@@ -0,0 +1,31 @@
+package com.example.songgallery;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TableLayout;
+
+public class Album_List extends Fragment {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_album__list, container, false);
+
+        return v;
+    }
+
+
+}
--- app/src/main/java/com/example/songgallery/MainActivity.java
+++ app/src/main/java/com/example/songgallery/MainActivity.java
@@ -1,16 +1,23 @@
 package com.example.songgallery;
 
+import static android.app.PendingIntent.getActivity;
+
+import android.app.Fragment;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
-import java.util.Objects;
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
-public class MainActivity extends AppCompatActivity {
+
+    private Spinner spinner;
+    private String selectedItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +30,44 @@ public class MainActivity extends AppCompatActivity {
 //            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
 //            return insets;
 //        });
+
+
+        spinner = findViewById(R.id.FilterSpinner);
+
+        // Create an ArrayAdapter using the string array and a default spinner layout.
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.Filter_By, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears.
+        adapter.setDropDownViewResource(R.layout.custom_spinner_item);
+        // Apply the adapter to the spinner.
+        spinner.setAdapter(adapter);
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        selectedItem = spinner.getSelectedItem().toString();
+        switch (selectedItem) {
+            case "artist":
+                Artist_List artist_list = new Artist_List();
+                getSupportFragmentManager().beginTransaction().replace(R.id.artist_list, artist_list).addToBackStack(null).commit();
+
+                break;
+            case "album":
+                Album_List album_list = new Album_List();
+                getSupportFragmentManager().beginTransaction().replace(R.id.album_list, album_list).addToBackStack(null).commit();
+                break;
+            case "track":
+
+                break;
+        }
+
+
+    }
+
+
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
 
     }
 }
